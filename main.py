63ad6f551d7a3e912cf58365c60da26453b29818
scottsc0de <scott5902@gmail.com>
2023-10-25 17:01:17 -0400

    added functions

---
+++ main.py
@@ -0,0 +1,27 @@
+# calculator functions w/ arithmetic operators in Python
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+def remainder(x, y):
+    return x % y
+
+
+def exponent(x, y):
+    return x**y
+
+
+def floor_div(x, y):
+    return x // y
